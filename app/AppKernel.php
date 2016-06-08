ef330eaeeff87eac13386ab07cec739cd86c4f02
s-miehlbradt <sabine.miehlbradt@scrutinizer-ci.com>
2016-06-08 17:22:50 +0200

    Update AppKernel.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -47,4 +47,8 @@ class AppKernel extends Kernel
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
+    
+    public function trouble() {
+        return 11;
+    }
 }
